f7968710138974b4379d362a2f1d0a01d2bc6117
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-09-15 16:09:46 -0300

    new branch

--- source/mode.py
+++ source/mode.py
@@ -3,10 +3,3 @@ def price(valor, porcentagem=25.001):
     porcentagem = float(porcentagem)
     preço_de = porcentagem * preço_por / 100
     return preço_por + preço_de
-
-def is_float(numero):
-    try:
-        float(numero)
-        return True
-    except:
-        return False
--- source/window.py
+++ source/window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = Tk()
 
@@ -21,15 +22,38 @@ class Apliccation():
         self.root.geometry("480x450")
         self.root.resizable(False, False)
 
+        self.background_image = Image.open("logo.png")
+        self.resized_bg = self.background_image.resize((260,200))
+        self.bg_image = ImageTk.PhotoImage(self.resized_bg)
+        self.bg_image_label = Label(self.root, image=self.bg_image, bd=0, bg="#000000")
+        self.bg_image_label.place(relheight=0.20, relwidth=1, anchor=CENTER, relx=0.5, rely=0.1)
+
     ## FRAME PRINCIPAL
     def frame(self):
         self.frame1 = Frame(self.root, relief="solid", bg="#4F4F4F")
-        self.frame1.place(relx=0.025, rely=0.010, relheight= 0.98,relwidth=0.95)
+        self.frame1.place(relx=0.5, rely=0.59, relheight= 0.78,relwidth=0.95, anchor=CENTER)
+
+        self.str = Label(self.frame1, text="Calculadora automática de preços comerciais para Marketplaces", bg="#4F4F4F", font=("verdana", 9, "bold"))
+        self.str.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.027, anchor=CENTER)
+        self.inst = Label(self.frame1, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.inst.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.10, anchor=CENTER)
+        self.instentry = Label(self.frame1, text="Preço final", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.instentry.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.28, anchor=CENTER)
+
+    
+    def on_click(self, arg):
+        arg.widget.config(bg="#D3D3D3")
+    def on_release(self, arg):
+        arg.widget.config(bg="#FFFF00")
+
     
     ##BOTÃO DE CALCULO 
     def buttons(self):
-        self.action_bt = Button(self.frame1, text="Calcular")
-        self.action_bt.place(relx=0.2, rely=0.67, relheight=0.15, relwidth=0.15)
+        self.action_bt = Button(self.frame1, text="Calcular", bg="#DCDCDC", bd=0)
+        self.action_bt.place(relx=0.2, rely=0.67, relheight=0.25, relwidth=0.25)
+
+        self.action_bt.bind("<Button-1>", self.on_click)
+        self.action_bt.bind("<ButtonRelease-1>", self.on_click)
         
     ## DEFININDO BOTÃO DE LIMPEZA
     def del_button(self):
@@ -39,7 +63,7 @@ class Apliccation():
                         bd=1, 
                         bg="#4F4F4F"
                         )
-        self.del_bt.place(relx=0.87, rely=0.82, relheight=0.09, relwidth=0.09)
+        self.del_bt.place(relheight=0.09, relwidth=0.09,relx=0.5, rely=0.94, anchor=CENTER)
        
     ## DEFININDO DEMAIS BOTÕES
     def checkbutton(self): 
@@ -53,7 +77,7 @@ class Apliccation():
                         highlightthickness=0, 
                         bg="#4F4F4F", 
                         activebackground="#4F4F4F", 
-                        font="bold",
+                        font=("verdana",14,"bold"),
                         )
         self.check2 = Radiobutton(
                         self.frame1, 
@@ -64,20 +88,20 @@ class Apliccation():
                         highlightthickness=0, 
                         bg="#4F4F4F", 
                         activebackground="#4F4F4F", 
-                        font="bold"
+                        font=("verdana",14,"bold")
                         )
-        self.check.place(relx=0.20, rely=0.35, relheight= 0.12 ,relwidth=0.12)
-        self.check2.place(relx=0.35, rely=0.35, relheight= 0.12 ,relwidth=0.12)
+        self.check.place(relheight= 0.12 ,relwidth=0.22, relx=0.34, rely=0.19, anchor=CENTER)
+        self.check2.place(relheight= 0.12 ,relwidth=0.22, relx=0.65, rely=0.19, anchor=CENTER)
         
     ## ENTRADA QUE RECEBE O VALOR
     def entry(self):
         self.entrycalc = Entry(
                         self.frame1, 
                         bd=0, 
-                        font="bold"
+                        font=("verdana", 15, "bold")
                         )
         self.entrycalc.icursor(0)
-        self.entrycalc.place(relx=0.46, rely=0.56, relheight= 0.1 ,relwidth=0.15)
+        self.entrycalc.place(relheight= 0.1 ,relwidth=0.22, relx=0.5, rely=0.38, anchor=CENTER)
 
     ## LABEL QUE RETORNA O RESULTADO
     def label(self): 
