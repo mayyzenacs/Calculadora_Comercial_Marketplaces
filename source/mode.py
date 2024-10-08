2df68d74284107232caf83c8b9b9c05d5d5008f3
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-10-08 07:21:16 -0300

    new

--- source/mode.py
+++ source/mode.py
@@ -3,7 +3,7 @@
 def calc(option, value):
     try: 
         valor_final = float(value)
-        if option == 1: 
+        if option == 0: 
             percent = 15.001
             
             percent = float(percent)
@@ -11,7 +11,7 @@ def calc(option, value):
             var1 = de_por + valor_final
             print(f"{var1:.2f}")
             return f'{var1:.2f}'
-        elif option == 0:  
+        elif option == 1:  
             percent_20 = 25.001
             percent_20 = float(percent_20)
             de_por = percent_20 * valor_final / 100
@@ -26,28 +26,4 @@ def calc(option, value):
             return f'{var3:.2f}'
     except ValueError:
         return "error"
-        
 
-# codigo antigo abaixo
-
-"""
-#o preço que queremos + 25,000%
-
-def price(valor, porcentagem=25.001):
-    valor_final = float(valor)
-    porcentagem = float(porcentagem)
-    de_por = porcentagem * valor_final / 100
-    return de_por + valor_final
-
-def checador(valorx, entrada):
-    if valorx != entrada:
-        return print(f"=> {valorx + 0.01:.2f}")
-    else:
-        pass
-
-entrada = input("Valor ").replace(",",".")
-resultado = price(entrada)
-checador(resultado, entrada)
-print(f"=> {resultado:.2f}")
-"""
-        
--- source/window.py
+++ source/window.py
@@ -33,21 +33,22 @@ class Apliccation():
 
     ## FRAME PRINCIPAL
     def frame(self):
-        self.frame1 = Frame(self.root, relief="solid", bg="#4F4F4F")
-        self.frame1.place(relx=0.5, rely=0.59, relheight= 0.78,relwidth=0.95, anchor=CENTER)
+        self.frame_back = Frame(self.root, relief="solid", bg="#4F4F4F")
+        self.frame_back.place(relx=0.5, rely=0.59, relheight= 0.78,relwidth=0.95, anchor=CENTER)
 
         self.str = Label(
-                        self.frame1, 
+                        self.frame_back, 
                         text="Calculadora automática de preços comerciais para Marketplaces", 
                         bg="#4F4F4F", 
                         font=("verdana", 9, "bold")
                         )
         self.str.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.035, anchor=CENTER)
-        self.inst = Label(self.frame1, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
-        self.inst.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.10, anchor=CENTER)
-        self.instentry = Label(self.frame1, text="Preço final", bg="#4F4F4F", font=("Verdana", 11, "bold"))
-        self.instentry.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.28, anchor=CENTER)
-
+        self.choice = Label(self.frame_back, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.choice.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.10, anchor=CENTER)
+        self.result_text = Label(self.frame_back, text="Preço final", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.result_text.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.28, anchor=CENTER)
+        self.checker = Label(self.frame_back, text="Checador", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.checker.place(relx=0.3, rely=0.4, relwidth=1, relheight=0.06, anchor="w")
 
     
     def on_click(self, arg):
@@ -58,7 +59,7 @@ class Apliccation():
     ## DEFININDO BOTÃO DE LIMPEZA
     def del_button(self):
         self.del_bt = Button(
-                        self.frame1, 
+                        self.frame_back, 
                         text="Clear", 
                         bd=1, 
                         bg="#4F4F4F"
@@ -69,8 +70,8 @@ class Apliccation():
     def checkbutton(self): 
         self.var = IntVar()
         self.check = Radiobutton(
-                        self.frame1, 
-                        text="20%", 
+                        self.frame_back, 
+                        text="10%", 
                         variable=self.var, 
                         value=0, 
                         bd=0, 
@@ -80,8 +81,8 @@ class Apliccation():
                         font=("verdana",14,"bold"),
                         )
         self.check2 = Radiobutton(
-                        self.frame1, 
-                        text="10%", 
+                        self.frame_back, 
+                        text="20%", 
                         variable=self.var, 
                         value=1, 
                         bd=0, 
@@ -91,7 +92,7 @@ class Apliccation():
                         font=("verdana",14,"bold")
                         )
         self.check3 = Radiobutton(
-                        self.frame1, 
+                        self.frame_back, 
                         text="35%", 
                         variable=self.var, 
                         value=2, 
@@ -109,7 +110,7 @@ class Apliccation():
     ## ENTRADA QUE RECEBE O VALOR
     def entry(self):
         self.entrycalc = Entry(
-                        self.frame1, 
+                        self.frame_back, 
                         bd=0, 
                         font=("verdana", 15, "bold")
                         )
@@ -126,28 +127,13 @@ class Apliccation():
         variavel = StringVar()
         variavel.set(self.test)
         print(option)
-        self.label2 = Label(self.frame1, textvariable = variavel, font=("verdana", 15, "bold"))
-        self.label2.place(relx=0.25, rely=0.56, relheight=0.25, relwidth=0.30)
-
-    def copy_button(self):
-        texto_copiar = self.test
-        self.root.clipboard_clear()
-        self.root.clipboard_append(texto_copiar)
-
-        self.copy_bt = Button(self.frame1, text="Copiar", bg="#DCDCDC", bd=0, command= self.copy_button)
-        self.copy_bt.place(relx=0.035, rely=0.75, relheight=0.15, relwidth=0.15)
-
-        self.copy_msg = Label(self.frame1, text="")
-        self.copy_msg.place(relx=0.2, rely=0.8, relheight=0.1, relwidth=0.4)
-
-        self.copy_bt.bind("<Button-1>", lambda: self.copy_msg.config(text="texto copiado"))
-        self.copy_bt.bind("<ButtonRelease-1>", lambda: self.copy_msg.config(text=""))
-
+        self.label2 = Label(self.frame_back, textvariable = variavel, font=("verdana", 15, "bold"))
+        self.label2.place(relx=0.25, rely=0.56, relheight=0.25, relwidth=0.30)        
 
     ##BOTÃO DE CALCULO 
     def buttons(self):
-        self.action_bt = Button(self.frame1, text="Calcular", bg="#DCDCDC", bd=0, command=self.take)
-        self.action_bt.place(relx=0.035, rely=0.56, relheight=0.15, relwidth=0.15)
+        self.action_bt = Button(self.frame_back, text="Calcular", bg="#DCDCDC", bd=0, command=self.take)
+        self.action_bt.place(relx=0.069, rely=0.51, relheight=0.15, relwidth=0.15)
 
         #ARRUMAR POR ULTIMO !!!!!!!!!!!!!
         self.action_bt.bind("<Button-1>", self.on_click)
@@ -157,7 +143,7 @@ class Apliccation():
     def label(self): 
         var = StringVar()
         label = Label(
-                        self.frame1, 
+                        self.frame_back, 
                         textvariable=var,
                         relief=RAISED, 
                         bg="#4F4F4F",
@@ -165,7 +151,21 @@ class Apliccation():
                         bd = 0
                     )
         var.set("PREÇO DE")
-        label.place(relx=0.55, rely=0.45, relheight= 0.1 ,relwidth=0.29, anchor='ne')
+        label.place(relx=0.55, rely=0.45, relheight= 0.1 ,relwidth=0.31, anchor='ne')
+    
+    def copy_button(self):
+        texto_copiar = self.test
+        self.root.clipboard_clear()
+        self.root.clipboard_append(texto_copiar)
+
+        self.copy_bt = Button(self.frame_back, text="Copiar", bg="#DCDCDC", bd=0, command= self.copy_button)
+        self.copy_bt.place(relx=0.069, rely=0.69, relheight=0.15, relwidth=0.15)
+
+        self.copy_msg = Label(self.frame_back, text="", bg="#4F4F4F")
+        self.copy_msg.place(relx=0.27, rely=0.82, relheight=0.05, relwidth=0.25)
+
+        self.copy_bt.bind("<Button-1>", lambda arg: self.copy_msg.config(text="texto copiado"))
+        self.copy_bt.bind("<ButtonRelease-1>", lambda arg: self.root.after(10000, lambda: self.copy_msg.config(text="")))
 
 
 Apliccation()
