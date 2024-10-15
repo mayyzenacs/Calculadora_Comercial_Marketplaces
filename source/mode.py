1707ddaf2e13eda97ee1db0bd6ab121f39ce5e35
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-10-15 07:50:40 -0300

    .

--- source/mode.py
+++ source/mode.py
@@ -1,21 +1,24 @@
 
 
 def calc(option, value):
+    if value is None or value == '':
+        return ''
+            
     try: 
         valor_final = float(value)
         if option == 0: 
             percent = 15.001
             
             percent = float(percent)
-            de_por = percent * valor_final / 100
-            var1 = de_por + valor_final
-            print(f"{var1:.2f}")
+            var1 = valor_final / 0.90
+            checker(0.10, var1)
             return f'{var1:.2f}'
         elif option == 1:  
             percent_20 = 25.001
             percent_20 = float(percent_20)
             de_por = percent_20 * valor_final / 100
             var2 = de_por + valor_final
+            checker(0.20, var2)
             return f'{var2:.2f}'
         elif option == 2:
             percent_35= 53.848
@@ -23,7 +26,11 @@ def calc(option, value):
             de_por = percent_35 * valor_final / 100
             var3 = de_por + valor_final
             print(f"{var3:.2f}")
+            checker(0.35, var3)
             return f'{var3:.2f}'
     except ValueError:
         return "error"
 
+def checker(percent, var_value):
+    check_value = (var_value * percent) - var_value
+    return print(check_value)
