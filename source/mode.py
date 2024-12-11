6c8c60a6c0ca821b3652470f2310654787356c0f
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-12-10 21:31:58 -0300

    new feature

--- source/mode.py
+++ source/mode.py
@@ -1,6 +1,8 @@
 
+
 ## DEFININDO CLASSE DE CÁLCULO
 class Calculator():
+
     def __init__(self):
         self.valor_final = 0
         self.percent = 0
@@ -9,6 +11,7 @@ class Calculator():
     
     ## FUNÇÃO QUE REALIZA O CALCULO PRINCIPAL
     def calc(self, option, value):
+
         if value is None or value == '':
             return ''
                 
@@ -16,23 +19,19 @@ class Calculator():
             self.valor_final = float(value)
 
             if option == 0: 
-                self.percent = 10
-                
+                self.percent = 10 
                 self.var = self.valor_final / 0.9
 
                 return f'{self.var:.2f}'
 
             elif option == 1:  
                 self.percent = 20
-                
                 self.var = self.valor_final / 0.8
-                
 
                 return f'{self.var:.2f}'
 
             elif option == 2:
                 self.percent = 35
-                self.percent = float(self.percent)
                 self.var = self.valor_final / 0.65
 
                 return f'{self.var:.2f}'
@@ -43,10 +42,12 @@ class Calculator():
         finally:
             self.checker()
 
+    ## FUNÇÃO QUE CHECA SE O CALCULO ESTÁ CORRETO
     def checker(self):
         check_value = self.var * (1 - self.percent / 100)
         return check_value
     
+    ## FUNÇÃO QUE CALCULO O VALOR PARA OFERTA MELI
     def offer(self):
         return self.valor_final - (self.valor_final * 0.03) 
     
--- source/window.py
+++ source/window.py
@@ -1,3 +1,5 @@
+
+## IMPORTANDO BIBLIOTECAS IMPORTANTES
 from tkinter import *
 from PIL import Image, ImageTk
 from mode import Calculator
@@ -7,7 +9,9 @@ from encoded_image import encoded_logo
 
 root = Tk()
 
+## INICIALIZAÇÃO 
 class Apliccation(): 
+
     def __init__(self):
         self.root = root
         self.calc = Calculator()
@@ -25,6 +29,7 @@ class Apliccation():
         self.logo()
         root.mainloop()
     
+
     ## DEFININDO FUNDO PRINCIPAL
     def screen(self):
         self.root.title("Calculadora de Preço Comercial Auto by Mayra Pereira")
@@ -33,11 +38,13 @@ class Apliccation():
         self.root.iconbitmap("icon.ico")
         self.root.resizable(False, False)
 
+
     ## DEFININDO O LOGO NO TOPO
     def logo(self):
         self.bg_image = PhotoImage(data=encoded_logo)
         self.bg_image_label = Label(self.root, image=self.bg_image, bd=0, bg="#000000")
         self.bg_image_label.place(relheight=0.20, relwidth=1, anchor=CENTER, relx=0.5, rely=0.1)
+
 
     ## FRAME PRINCIPAL
     def frame(self):
@@ -52,6 +59,7 @@ class Apliccation():
                         )
         self.str.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.035, anchor=CENTER)
        
+
     ## DEFININDO RADIO BUTTONS DE OPÇÃO
     def checkbutton(self): 
         self.choice = Label(self.frame_back, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
@@ -119,6 +127,7 @@ class Apliccation():
         self.checker_label(self.calc.checker())
         self.offer_label()
 
+
         ## POSICIONA O VALOR FINAL NO FRAME
         result = StringVar()
         result.set(self.return_result)
@@ -130,6 +139,7 @@ class Apliccation():
     def buttons(self):
         self.action_bt = Button(self.frame_back, text="Calcular", bg="#DCDCDC", bd=0, command=self.take)
         self.action_bt.place(relx=0.069, rely=0.51, relheight=0.15, relwidth=0.15)
+
 
     ## LABEL QUE RETORNA O RESULTADO
     def label(self): 
@@ -145,13 +155,16 @@ class Apliccation():
         var.set("PREÇO DE")
         label.place(relx=0.55, rely=0.45, relheight= 0.1 ,relwidth=0.31, anchor='ne')
         
+
         ## DEFINE O CHECADOR
         self.checker = Label(self.frame_back, text="Checador", bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.checker.place(relx=0.59, rely=0.49, relwidth=0.4, relheight=0.06, anchor="w")
 
+
         ## DEFINE O TEXTO DE OFERTA
         self.value_checker = Label(self.frame_back, text="-3%", bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.value_checker.place(relx=0.63, rely=0.69, relwidth=0.34, relheight=0.06, anchor="w")
+
 
     ## DEFININDO LABEL DO CHECKER DE VALOR
     def checker_label(self, checker_value):
@@ -161,13 +174,15 @@ class Apliccation():
         self.check_value = Label(self.frame_back, textvariable = checker_result, font=("verdana", 15, "bold"))
         self.check_value.place(relx=0.69, rely=0.58, relwidth=0.2, relheight=0.1, anchor="w")
 
+
     ## DEFININDO LABEL DE OFERTA
     def offer_label(self):
         offer_value = StringVar()
         offer_value.set(f"{self.calc.offer():.2f}")
         self.offer_price = Label(self.frame_back, textvariable=offer_value , bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.offer_price.place(relx=0.63, rely=0.78, relwidth=0.34, relheight=0.06, anchor="w")
-    
+
+
     ## DEFININDO O BOTÃO DE COPIAR
     def copy_text(self):
         self.copy_bt = Button(self.frame_back, text="Copiar", bg="#DCDCDC", bd=0, command= self.copy_button)
@@ -179,11 +194,13 @@ class Apliccation():
         self.copy_bt.bind("<Button-1>", lambda arg: self.copy_msg.config(text="texto copiado"))
         self.copy_bt.bind("<ButtonRelease-1>", lambda arg: self.root.after(1000, lambda: self.copy_msg.config(text="")))
 
+
     ## FUNÇÃO DE USO DO BOTÃO COPIAR
     def copy_button(self):
         copy = self.return_result
         self.root.clipboard_clear()
         self.root.clipboard_append(copy)
+
 
     ## DEFININDO BOTÃO DE LIMPEZA
     def del_button(self):
@@ -196,12 +213,12 @@ class Apliccation():
                         )
         self.del_bt.place(relheight=0.09, relwidth=0.09,relx=0.5, rely=0.94, anchor=CENTER)
 
+
     ## FUNÇÃO DO BOTÃO LIMPAR
     def clear(self):
         self.entrycalc.delete(0, END)
         
-    
-    
+     
 Apliccation()
 
     
