61d2ea031f9813fd8c61a89dec54ef04b29b266b
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-01-10 07:33:02 -0300

    new 1.0

--- source/window.py
+++ source/window.py
@@ -1,12 +1,10 @@
 
 ## IMPORTANDO BIBLIOTECAS IMPORTANTES
-from tkinter import *
+import tkinter as tk
 from mode import Calculator
-
+from PIL import Image
 import os
 from encoded_image import encoded_logo
-
-root = Tk()
 
 ## INICIALIZAÇÃO 
 class Apliccation(): 
@@ -26,7 +24,6 @@ class Apliccation():
         self.copy_text()
         self.offer_label()
         self.logo()
-        root.mainloop()
     
 
     ## DEFININDO FUNDO PRINCIPAL
@@ -34,38 +31,37 @@ class Apliccation():
         self.root.title("Calculadora de Preço Comercial Auto by Mayra Pereira")
         self.root.configure(background= "#363636")
         self.root.geometry("480x450")
-        self.root.iconbitmap(r"C:\Users\May\Documents\GitHub\25.001_Auto\source\img\icon.ico")
         self.root.resizable(False, False)
 
 
     ## DEFININDO O LOGO NO TOPO
     def logo(self):
-        self.bg_image = PhotoImage(data=encoded_logo)
-        self.bg_image_label = Label(self.root, image=self.bg_image, bd=0, bg="#000000")
-        self.bg_image_label.place(relheight=0.20, relwidth=1, anchor=CENTER, relx=0.5, rely=0.1)
+        self.bg_image = tk.PhotoImage(data=encoded_logo)
+        self.bg_image_label = tk.Label(self.root, image=self.bg_image, bd=0, bg="#000000")
+        self.bg_image_label.place(relheight=0.20, relwidth=1, anchor=tk.CENTER, relx=0.5, rely=0.1)
 
 
     ## FRAME PRINCIPAL
     def frame(self):
-        self.frame_back = Frame(self.root, relief="solid", bg="#4F4F4F")
-        self.frame_back.place(relx=0.5, rely=0.59, relheight= 0.78,relwidth=0.95, anchor=CENTER)
+        self.frame_back = tk.Frame(self.root, relief="solid", bg="#4F4F4F")
+        self.frame_back.place(relx=0.5, rely=0.59, relheight= 0.78,relwidth=0.95, anchor=tk.CENTER)
 
-        self.str = Label(
+        self.str = tk.Label(
                         self.frame_back, 
                         text="Calculadora automática de preços comerciais para Marketplaces", 
                         bg="#4F4F4F", 
                         font=("verdana", 9, "bold")
                         )
-        self.str.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.035, anchor=CENTER)
+        self.str.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.035, anchor=tk.CENTER)
        
 
     ## DEFININDO RADIO BUTTONS DE OPÇÃO
     def checkbutton(self): 
-        self.choice = Label(self.frame_back, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
-        self.choice.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.10, anchor=CENTER)
+        self.choice = tk.Label(self.frame_back, text="Selecione qual porcentagem utilizar", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.choice.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.10, anchor=tk.CENTER)
 
-        self.var = IntVar()
-        self.check = Radiobutton(
+        self.var = tk.IntVar()
+        self.check = tk.Radiobutton(
                         self.frame_back, 
                         text="10%", 
                         variable=self.var, 
@@ -76,7 +72,7 @@ class Apliccation():
                         activebackground="#4F4F4F", 
                         font=("verdana",14,"bold"),
                         )
-        self.check2 = Radiobutton(
+        self.check2 = tk.Radiobutton(
                         self.frame_back, 
                         text="20%", 
                         variable=self.var, 
@@ -87,7 +83,7 @@ class Apliccation():
                         activebackground="#4F4F4F", 
                         font=("verdana",14,"bold")
                         )
-        self.check3 = Radiobutton(
+        self.check3 = tk.Radiobutton(
                         self.frame_back, 
                         text="35%", 
                         variable=self.var, 
@@ -98,23 +94,23 @@ class Apliccation():
                         activebackground="#4F4F4F", 
                         font=("verdana",14,"bold")
                         )
-        self.check.place(relheight= 0.12, relwidth=0.22, relx=0.2, rely=0.19, anchor=CENTER)
-        self.check2.place(relheight= 0.12, relwidth=0.22, relx=0.5, rely=0.19, anchor=CENTER)
+        self.check.place(relheight= 0.12, relwidth=0.22, relx=0.2, rely=0.19, anchor=tk.CENTER)
+        self.check2.place(relheight= 0.12, relwidth=0.22, relx=0.5, rely=0.19, anchor=tk.CENTER)
         self.check3.place(relheight= 0.12, relwidth=0.22, relx=0.7, rely=0.19, anchor='w')
 
 
     ## DEFININDO A ENTRADA QUE RECEBE O VALOR
     def entry(self):
-        self.result_text = Label(self.frame_back, text="Preço final", bg="#4F4F4F", font=("Verdana", 11, "bold"))
-        self.result_text.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.28, anchor=CENTER)
+        self.result_text = tk.Label(self.frame_back, text="Preço final", bg="#4F4F4F", font=("Verdana", 11, "bold"))
+        self.result_text.place(relheight=0.09, relwidth=1, relx=0.5, rely=0.28, anchor=tk.CENTER)
 
-        self.entrycalc = Entry(
+        self.entrycalc = tk.Entry(
                         self.frame_back, 
                         bd=0, 
                         font=("verdana", 15, "bold")
                         )
         self.entrycalc.icursor(0)
-        self.entrycalc.place(relheight= 0.1 ,relwidth=0.22, relx=0.5, rely=0.38, anchor=CENTER)
+        self.entrycalc.place(relheight= 0.1 ,relwidth=0.22, relx=0.5, rely=0.38, anchor=tk.CENTER)
         
 
     # CHAMA O VALOR DO RATION BUTTON
@@ -128,25 +124,25 @@ class Apliccation():
 
 
         ## POSICIONA O VALOR FINAL NO FRAME
-        self.result = StringVar()
+        self.result = tk.StringVar()
         self.result.set(self.return_result)
-        self.label_return = Label(self.frame_back, textvariable = self.result, font=("verdana", 15, "bold"))
+        self.label_return = tk.Label(self.frame_back, textvariable = self.result, font=("verdana", 15, "bold"))
         self.label_return.place(relx=0.25, rely=0.56, relheight=0.25, relwidth=0.30)        
         
 
     ## INSERINDO BOTÃO DE CALCULO 
     def buttons(self):
-        self.action_bt = Button(self.frame_back, text="Calcular", bg="#DCDCDC", bd=0, command=self.take)
+        self.action_bt = tk.Button(self.frame_back, text="Calcular", bg="#DCDCDC", bd=0, command=self.take)
         self.action_bt.place(relx=0.069, rely=0.51, relheight=0.15, relwidth=0.15)
 
 
     ## LABEL QUE RETORNA O RESULTADO
     def label(self): 
-        var = StringVar()
-        label = Label(
+        var = tk.StringVar()
+        label = tk.Label(
                         self.frame_back, 
                         textvariable=var,
-                        relief=RAISED, 
+                        relief=tk.RAISED, 
                         bg="#4F4F4F",
                         font=("verdana", 15, "bold"),
                         bd = 0
@@ -156,38 +152,38 @@ class Apliccation():
         
 
         ## DEFINE O CHECADOR
-        self.checker = Label(self.frame_back, text="Checador", bg="#4F4F4F", font=("Verdana", 12, "bold"))
+        self.checker = tk.Label(self.frame_back, text="Checador", bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.checker.place(relx=0.59, rely=0.49, relwidth=0.4, relheight=0.06, anchor="w")
 
 
         ## DEFINE O TEXTO DE OFERTA
-        self.value_checker = Label(self.frame_back, text="-3%", bg="#4F4F4F", font=("Verdana", 12, "bold"))
+        self.value_checker = tk.Label(self.frame_back, text="-3%", bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.value_checker.place(relx=0.63, rely=0.69, relwidth=0.34, relheight=0.06, anchor="w")
 
 
     ## DEFININDO LABEL DO CHECKER DE VALOR
     def checker_label(self, checker_value):
         checker_result = ""
-        self.checker_result = StringVar()
+        self.checker_result = tk.StringVar()
         self.checker_result.set(checker_value)
-        self.check_value = Label(self.frame_back, textvariable = self.checker_result, font=("verdana", 15, "bold"))
+        self.check_value = tk.Label(self.frame_back, textvariable = self.checker_result, font=("verdana", 15, "bold"))
         self.check_value.place(relx=0.69, rely=0.58, relwidth=0.2, relheight=0.1, anchor="w")
 
 
     ## DEFININDO LABEL DE OFERTA
     def offer_label(self):
-        self.offer_value = StringVar()
+        self.offer_value = tk.StringVar()
         self.offer_value.set(f"{self.calc.offer():.2f}")
-        self.offer_price = Label(self.frame_back, textvariable=self.offer_value , bg="#4F4F4F", font=("Verdana", 12, "bold"))
+        self.offer_price = tk.Label(self.frame_back, textvariable=self.offer_value , bg="#4F4F4F", font=("Verdana", 12, "bold"))
         self.offer_price.place(relx=0.63, rely=0.78, relwidth=0.34, relheight=0.06, anchor="w")
 
 
     ## DEFININDO O BOTÃO DE COPIAR
     def copy_text(self):
-        self.copy_bt = Button(self.frame_back, text="Copiar", bg="#DCDCDC", bd=0, command= self.copy_button)
+        self.copy_bt = tk.Button(self.frame_back, text="Copiar", bg="#DCDCDC", bd=0, command= self.copy_button)
         self.copy_bt.place(relx=0.069, rely=0.69, relheight=0.15, relwidth=0.15)
 
-        self.copy_msg = Label(self.frame_back, text="", bg="#4F4F4F")
+        self.copy_msg = tk.Label(self.frame_back, text="", bg="#4F4F4F")
         self.copy_msg.place(relx=0.27, rely=0.82, relheight=0.05, relwidth=0.25)
 
         self.copy_bt.bind("<Button-1>", lambda arg: self.copy_msg.config(text="texto copiado"))
@@ -203,25 +199,27 @@ class Apliccation():
 
     ## DEFININDO BOTÃO DE LIMPEZA
     def del_button(self):
-        self.del_bt = Button(
+        self.del_bt = tk.Button(
                         self.frame_back, 
                         text="Clear", 
                         bd=1, 
                         bg="#4F4F4F",
                         command= self.clear
                         )
-        self.del_bt.place(relheight=0.09, relwidth=0.09,relx=0.5, rely=0.94, anchor=CENTER)
+        self.del_bt.place(relheight=0.09, relwidth=0.09,relx=0.5, rely=0.94, anchor=tk.CENTER)
 
 
     ## FUNÇÃO DO BOTÃO LIMPAR
     def clear(self):
-        self.entrycalc.delete(0, END)
+        self.entrycalc.delete(0, tk.END)
         self.checker_result.set("")
         self.offer_value.set("")
         self.result.set("")
         
         
-     
-Apliccation()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Apliccation()
+    root.mainloop()
 
     
--- source/window.spec
+++ source/window.spec
@@ -35,5 +35,4 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['img\\icon.ico.'],
 )
