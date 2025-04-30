145581914cd041af23750939f4af167dc6644cbb
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-04-29 22:20:30 -0300

    .

--- postgres.session.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE operations ENCODING 'UTF8';
-CREATE TABLE prices (
-    id SERIAL PRIMARY KEY,
-    percent VARCHAR(5) NOT NULL,
-    valor NUMERIC NOT NULL,
-    final_value NUMERIC NOT NULL,
-    date_time TIMESTAMP DEFAULT now()
-);
--- source/mode.py
+++ source/mode.py
@@ -1,21 +1,20 @@
 import math
 import psycopg2
 
-conection = psycopg2.connect(
-    database='postgres',
+connection = psycopg2.connect(
+    database='calculator',
     user='postgres',
     password='123',
     host='localhost',
-    port='5432',
-    options='-c client_encoding=UTF8'
+    port='5432'
 )
-cursor = conection.cursor()
+cursor = connection.cursor()
 
 ## DEFININDO CLASSE DE CÁLCULO
 class Calculator():
 
     def __init__(self):
-        self.final_value = 0
+        self.float_value = 0
         self.percent = 0
         self.value = 0
         self.rest = 0
@@ -27,52 +26,50 @@ class Calculator():
             return ''
         
         try: 
-            self.final_value = float(value)
+            self.float_value = float(value)
+            self.value = self.float_value
 
             if option == 0: 
                 discount = 1 - 0.10
-                var = self.final_value / discount
-                rest = math.ceil(var * 100) / 100
-                
-                return rest
 
             elif option == 1:  
                 discount = 1 - 0.20
-                var = self.final_value / discount
-                rest = math.ceil(var * 100) / 100
-                
-                return rest
 
             elif option == 2:
                 discount = 1 - 0.35
-                var = self.final_value / discount
-                rest = math.ceil(var * 100) / 100
+            
+
+            var = self.float_value / discount
+            self.rest = math.ceil(var * 100) / 100
                 
-                return rest
+            return self.rest
 
         except ValueError:
             return "error"
 
         finally:
             self.checker()
+            self.save_db()
 
     ## FUNÇÃO QUE CHECA SE O CALCULO ESTÁ CORRETO
     def checker(self):
-        check_value = (self.rest - self.final_value) - self.rest
+        check_value = (self.rest - self.float_value) - self.rest
         return abs(check_value)
     
     ## FUNÇÃO QUE CALCULO O VALOR PARA OFERTA MELI
     def offer(self):
-        return self.final_value - (self.final_value * 0.03) 
+        return self.float_value - (self.float_value * 0.03) 
     
 
     def save_db(self):
         cursor.execute(
-            "INSERT INTO operations (percent, value, rest) VALUES (%s, %s, %s)",
+            "INSERT INTO prices (choice, valor, final_value) VALUES (%s, %s, %s)",
             (self.percent, self.value, self.rest)
         )
-        conection.commit()
+        connection.commit()
         print("operação salva com sucesso no banco de dados")
+        connection.close()
+
 
 
     
